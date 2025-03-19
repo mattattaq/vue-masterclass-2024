3ca664191c8bcc9a230f6bb1efa18aa315e3e596
MattAttaq <mattatttaq@gmail.com>
2025-03-19 14:36:12 -0400

    [ chapter 4 - 36 ] Use Lucide Icons with Iconify and Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
 import { supabase } from './lib/supaBaseClient';
+import Input from './components/ui/input/Input.vue';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 console.log(supabase)
 </script>
 
 <template>
+  <nav class="h-16 border-b bg-mmuted/40 flex justify-between px-6 items-center">
+    <form class="w-full max-w-96">
+      <Input class="w-full bg-background pl-8" placeholder="Search..." type="text" name="" id="" />
+    </form>
+
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </nav>
   <main>
     <RouterView />
   </main>
